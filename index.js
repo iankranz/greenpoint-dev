74a1ee1abcf64694b16f9ba823dfef538b4bfd27
iankranz <ian@iankranz.com>
2023-08-26 11:56:37 -0400

    add more contact points

---
+++ index.js
@@ -0,0 +1,14 @@
+const accordions = document.querySelectorAll(".accordion")
+
+accordions.forEach((accordion) => {
+  accordion.addEventListener("click", () => {
+    accordion.classList.toggle("active")
+  })
+})
+
+const emailObf = "ian[at]greenpoint[dot]dev"
+const email = emailObf.replace("[at]", "@").replace("[dot]", ".")
+const href = `mailto:${email}`
+const emailLink = document.querySelector("#email-link")
+emailLink.setAttribute("href", href)
+emailLink.textContent = email
